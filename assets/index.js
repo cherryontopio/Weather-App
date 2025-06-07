66f980d82683764be64c8ff83e16fe5bd0f05387
cherryontopio <ivetchadim@gmail.com>
2025-06-07 07:15:33 +0100

    Added days for forecast, added javascript

--- assets/index.js
+++ assets/index.js
@@ -52,7 +52,32 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+     <div class="forecast-days">
+      <div class="forecast-day">Tue</div>
+      <div class="forecast-icon">sunny</div>
+      <div class="forecast-temp">
+        <div class="forecast-temp-day">13</div>
+        <div class="forecast-temp-night">11</div>
+      </div>
+     </div>
+     `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("London");
+
+displayForecast();
